35987ee779f00050955077b93fdecaf48cd9b1c5
Alexander <lungor77@gmail.com>
2023-08-08 17:48:02 +0300

    Added GLAD library

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,6 @@
+#include <glad/glad.h>
 #include <GLFW/glfw3.h>
+#include <iostream>
 
 int main(void)
 {
@@ -19,11 +21,27 @@ int main(void)
     /* Make the window's context current */
     glfwMakeContextCurrent(window);
 
+    /*
+	if(gladLoadGL()){
+		std::cout << "Cant load GLAD" << std::endl;
+		return -1;
+	}
+	*/
+	
+	if (!gladLoadGLLoader((GLADloadproc) glfwGetProcAddress)) {
+        std::cout << "Failed to initialize OpenGL context" << std::endl;
+        return -1;
+    }
+	
+	std::cout << "OpenGL " << GLVersion.major << "." << GLVersion.minor << std::endl;
+	
+	glClearColor(1, 1, 0, 1);
+	
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
     {
         /* Render here */
-        //glClear(GL_COLOR_BUFFER_BIT);
+        glClear(GL_COLOR_BUFFER_BIT);
 
         /* Swap front and back buffers */
         glfwSwapBuffers(window);
